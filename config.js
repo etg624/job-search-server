0c2ddc648f0aeee6ad14afa9acf9ce2e86c57d8f
Evan Guirino <etg624@gmail.com>
2019-07-18 14:46:44 -0700

    changed DB_URL

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-exports.DATABASE_URL =
-  process.env.DATABASE_URL ||
-  'mongodb://evan:mila-is-my-love12@ds237947.mlab.com:37947/searchule';
+exports.DATABASE_URL = process.env.DATABASE_URL;
 
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
